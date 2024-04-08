76afdf05dd59adfda8b1996613d6e3c0e5c82468
OrionWz <158779705+OrionWz@users.noreply.github.com>
2024-04-08 15:36:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
 
-with open(r"C:\Users\banks\OneDrive\Documents\nell_file.json", "r") as read_file:
+with open(r"C:\Users\orion\OneDrive\Documents\orion_file.json", "r") as read_file:
     data = json.load(read_file)
 
 result = [json.dumps(record) for record in data]
-with open(r"C:\Users\banks\OneDrive\Documents\nell_file.json", 'w') as obj:
+with open(r"C:\Users\banks\OneDrive\Documents\orion_file.json", 'w') as obj:
     for i in result:
         obj.write(i + '\n')

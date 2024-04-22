e7ff663a87cf17bcd22294d40dbd7a19a9927db5
OrionWz <158779705+OrionWz@users.noreply.github.com>
2024-04-22 12:47:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -28,3 +28,17 @@ with Flow("Process JSON File") as flow:
     write_json_file(processed_data, file_path_out)
 
 flow.run()
+
+
+import json
+
+with open(r"C:\Users\banks\OneDrive\Documents\nell_file.json", "r") as read_file:
+    data = json.load(read_file)
+
+result = [json.dumps(record) for record in data]
+with open(r"C:\Users\banks\OneDrive\Documents\nell_file.json", 'w') as obj:
+    for i in result:
+        obj.write(i + '\n')
+
+import json 
+with open(r"C:\
